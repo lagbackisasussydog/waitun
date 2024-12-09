aa4f5101e861ff39e5c89ac7280bd7d663c2b272
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2024-12-09 21:56:38 +0700

    tween.lua

--- tween.lua
+++ tween.lua
@@ -1,5 +1,12 @@
 local tween = {}
 
-local 
+local plr = game.Players.LocalPlayer
+local char = plr.Character
+
+function tween.Add(name,inst,property)
+  local svc = game:GetService("TweenService")
+
+  svc:Create(inst,TweenInfo.new(),property):Play()
+end
 
 return tween
