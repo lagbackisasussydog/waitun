51ce615692966744d7ae32911fc8c8c3685aba8f
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2024-12-11 16:40:52 +0700

    main.lua

--- main.lua
+++ main.lua
@@ -41,3 +41,42 @@ while wait(.1) do
         until v.Humanoid.Health == 0
     end
 end
+
+local plrgui = game.Players.LocalPlayer.PlayerGui
+
+local e = false
+
+local gui = Instance.new("ScreenGui",plr.PlayerGui)
+gui.ResetOnSpawn = false
+
+local btn = Instance.new("TextButton",gui)
+btn.Active = true
+btn.Draggable = true
+
+btn.Size = UDim2.new(0,50,0,50)
+btn.BackgroundColor3 = Color3.fromRGB(255,0,0)
+btn.TextColor3 = Color3.fromRGB(255,255,255)
+btn.TextScaled = true
+btn.Text = "Stop"
+
+btn.MouseButton1Up:Connect(function()
+    e = not e
+
+    if e then
+        btn.Text = "Start"
+        getscript().Enabled = false
+        btn.BackgroundColor3 = Color3.fromRGB(0,255,0)
+    else
+        btn.Text = "Stop"
+        getscript().Enabled = true
+        btn.BackgroundColor3 = Color3.fromRGB(255,0,0)
+    end
+end)
+
+char.Humanoid.HealthChanged:Connect(function(h)
+    if h == 0 then
+        e = true
+        task.wait(10)
+        e = false
+    end
+end)
