da3dee3a4edef797dae60ce1455db56c012d0773
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2024-12-29 20:58:53 +0700

    AutoFarm.lua

--- Waitun/Modules/AutoFarm.lua
+++ Waitun/Modules/AutoFarm.lua
@@ -1,5 +1,5 @@
 local function load()
-	if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+    if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
     
     local rs = game:GetService("ReplicatedStorage")
     local tw = game:GetService("TweenService")
@@ -12,7 +12,9 @@ local function load()
     local net = rs.Modules.Net
 
     local e = Enum
-    
+		local ef = workspace.Enemies
+		local map = workspace.Map
+		
     repeat
       wait()
     until c
@@ -71,6 +73,25 @@ local function load()
           end
       end
     end
-    
+
+		while wait(.1) do
+			pcall(function()
+				for i,enemy in ef:GetChildren() do
+					if (getPLevel(p) == 1 or getPLevel(p) < 300) and (e.Name == "Sky Bandit [Lv. 150]" or e.Name == "Dark Master [Lv. 175"]) then
+							disableCollide(c)
+
+						if #ef:GetChildren() == 0 then
+							repeat
+									wait()
+							until #ef:GetChildren() ~= 0
+						end
+					elseif (getPLevel(p) == 1 or getPLevel(p) < 300) and e.Name == "Military Soldier [Lv. 300]" then
+
+					end	
+				end
+			end)
+		end
   end
 end
+
+coroutine.wrap(load)()
