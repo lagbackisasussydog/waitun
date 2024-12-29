0bdced0b83990377d40562266396379d2fa65fd8
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2024-12-29 20:49:04 +0700

    AutoFarm.lua

--- Waitun/Modules/AutoFarm.lua
+++ Waitun/Modules/AutoFarm.lua
@@ -1,1 +1,76 @@
+local function load()
+	if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+    
+    local rs = game:GetService("ReplicatedStorage")
+    local tw = game:GetService("TweenService")
 
+    local p = game.Players.LocalPlayer
+    local c = p.Character
+    local r = c.PrimaryPart
+
+    local cmf = rs.Remotes.CommF_
+    local net = rs.Modules.Net
+
+    local e = Enum
+    
+    repeat
+      wait()
+    until c
+
+    local function inst(inst : String,place) return Instance.new(inst,place) end
+    
+    local att = inst("Attachment",r)
+    att.Name = "Force"
+
+    local align = inst("AlignPosition",att)
+    align.Mode = e.PositionAlignmentMode.OneAttachment
+    align.MaxForce = math.huge
+    align.MaxVelocity = math.huge
+    align.Position = r.Position
+    
+    local function Tween(inst,info,prop)
+      local track = tw:Create(inst,info,prop)
+      track:Play()
+      track.Completed:Wait()
+    end
+
+    local function getPLevel(pl)
+      return pl.Data.Level.Value
+    end
+
+    local function tp(cf : CFrame)
+      local tped = inst("BoolValue",r)
+      tped = false
+      
+      if tped then return end
+
+      tped = true
+      Tween(r,TweenInfo.new(30,e.EasingStyle.Linear),{CFrame = cf})
+    end
+
+    local function unTp()
+      local tped = r:FindFirstChild("BoolValue")
+
+      tped = false
+    end
+
+    local function fireEvent(model : Model)
+      local regHit = net["RE/RegisterHit"]
+
+      for _,part in model:GetChildren() do
+        if part:IsA("BasePart") then
+          regHit:FireServer(part)
+        end
+      end
+    end
+
+    local function disableCollide(char : Model)
+        for _,part in char:GetChildren() do
+          if part:IsA("BasePart") then
+            part.CanCollide = false
+          end
+      end
+    end
+    
+  end
+end
