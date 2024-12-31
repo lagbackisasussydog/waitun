439220fbb971786f191119eebd1891c8f7974af1
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2024-12-31 21:13:12 +0700

    main.lua

--- Waitun/main.lua
+++ Waitun/main.lua
@@ -14,6 +14,12 @@ local function load()
         ["Fruit"] = {},
         ["Raid"] = {},
         ["Teleport"] = {},
+        
+        ["Place"] = {
+            ["First Sea"] = false,
+            ["Second Sea"] = false,
+            ["Third Sea"] = false,
+        },
     } 
     local rs  = game:GetService("ReplicatedStorage")
     local vim = game:GetService("VirtualInputManager")
@@ -24,12 +30,17 @@ local function load()
     local map     = w.Map
     local chests  = w.ChestModels
     
+    local cmf = rs.Remotes.CommF_
+    local net = rs.Modules.Net
+    
+    cmf:InvokeServer("SetTeam","Pirates")
+    repeat
+        wait()
+    until game.Players.LocalPlayer.Character
+    
     local p = game.Players.LocalPlayer
     local c = p.Character
     local r = c.PrimaryPart
-    
-    local cmf = rs.Remotes.CommF_
-    local net = rs.Modules.Net
     
     local function tp(inst,info,prop)
         local t = tw:Create(inst,info,prop)
@@ -41,38 +52,33 @@ local function load()
         vim:SendMouseButtonEvent(0,0,0,true,game,1)
     end
     
-    local function MobAura()
-    
-        local enabled = Configs.Grinder.MobGrinder.Enabled
-        
+    local function MobAura(e)
         local force = Instance.new("BodyVelocity",r)
         force.Name  = "Force"
         
-        if enabled == true then
+        if e == true then
             force.MaxForce = Vector3.new(math.huge,math.huge,math.huge)
-            repeat
-                wait(.5)
-                pcall(function()
-                    for _,e in enemies:GetChildren() do
-                        local er = e.PrimaryPart
-                        local eh = e.Humanoid
+            pcall(function()
+              for _,e in enemies:GetChildren() do
+                  if e == false then break end
+                  local er = e.Head
+                  local eh = e.Humanoid
                         
-                        local dist = p:DistanceFromCharacter(er.Position)
+                  local dist = p:DistanceFromCharacter(er.Position)
                         
-                        er.Size     = Vector3.new(50,50,50)
-                        er.Anchored = true
-                        tp(r,TweenInfo.new(dist / 350,Enum.EasingStyle.Linear),{CFrame = er.CFrame * CFrame.new(0,30,0)
+                  er.Size     = Vector3.new(500,500,500)
+                  er.Anchored = true
+                  tp(r,TweenInfo.new(dist / 350,Enum.EasingStyle.Linear),{CFrame = er.CFrame * CFrame.new(0,30,0)})
                         
-                        repeat
-                            wait(.5)
-                            att(e)
-                        until eh.Health == 0
-                    end
-                end)
-            until enabled == false
-        else
-            force:Destroy()
-        end
+                  repeat
+                       wait(.5)
+                       att(e)
+                  until eh.Health == 0
+               end
+            end)
+       elseif e == false then
+          force:Destroy()
+      end
     
     end
     local Luxtl = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Luxware-UI-Library/main/Source.lua"))()
@@ -97,17 +103,25 @@ local function load()
     
     local MobAuraSection = GrinderTab:Section("Mob grinder")
     
-    MobAuraSection:Toggle("Enable",function(toggled)
-        Configs.Grinder.MobGrinder.Enabled = toggled
-        MobAura()
+    MobAuraSection:Toggle("Enable",function(t)
+        print(t)
+        coroutine.wrap(MobAura)(t)
     end)
+    
     MobAuraSection:DropDown("Weapon",{"Melee","Sword"},function(weapon)
         Configs.Grinder.MobGrinder.Weapon = weapon
     end)
+    
     MobAuraSection:DropDown("Mob",{},function()
     
     end)
 
+    local MapSection = TeleportTab:Section("Tween to map")
+    
+    MapSection:Toggle("Tween",function(t) end)
+
+    MapSection:DropDown("Map",map:GetChildren(),function(map) end)
+    
     local cf = creditsTab:Section("Main Credits")
     cf:Credit("Luxt: UI library")
 end
