32a829fb9f62f1361b4a1a6dcae8857683e86506
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2025-01-03 16:57:48 +0700

    main.lua

--- Waitun/main.lua
+++ Waitun/main.lua
@@ -13,9 +13,38 @@ local function load()
     local cmf = rs.Remotes.CommF_
     local net = rs.Modules.Net
 
-    if p.Team = game.Teams.Neutral then cmf:InvokeServer("SetTeam","Pirates") end
+    local second = 0
 
     repeat wait() until p.Character
+
+    local function v3(x,y,z)
+        return vector3.new(x,y,z)
+    end
+
+    local function att()
+        vim:SendMouseButtonEvent(0,0,0,true,game,1)
+    end
+
+    local function nocdtp(inst,info,prop)
+        local t = tw:Create(inst,info,prop)
+        t:Play()
+    end
+
+    local function tp(inst,info,prop)
+        local t = tw:Create(inst,info,prop)
+        t:Play()
+        t.Completed:Wait()
+    end
+
+    local function anchor(state)
+        if state then
+            local f = Instance.new("BodyVelocity",r)
+            f.Name = "F"
+            f.MaxForce = v3(1000000,10000000000,10000000000)
+        else
+            r:FindFirstChild("F"):Destroy()
+        end
+    end
 
     local c = p.Character
     local r = c.PrimaryPart
@@ -45,14 +74,32 @@ local function load()
     local MobGrinder = Tabs.Grinder:AddToggle("MobGrinder",{Title = "Enable mob grinder",Default = false})
 
     LevelGrinder:OnChanged(function()
-        print(opt.LevelGrinder.Value)
+        
     end)
 
     opt.LevelGrinder:SetValue(false)
 
     MobGrinder:OnChanged(function()
         while opt.MobGrinder.Value do
-            
+                local co = coroutine.create(function()
+                    for _,e in pairs(enemies:GetChildren()) do
+                        local head = e.Head
+                        local hum = e.Humanoid
+                        local dist = p:DistanceFromCharacter(head.Position)
+
+                        anchor(true)
+                        head.Size = v3(50,50,50)
+                        tp(r,TweenInfo.new(dist / 350),{CFrame = head.CFrame})
+
+                        repeat
+                            wait(.1)
+                            att()
+                        until hum.Heath == 0
+                    end
+                end)
+
+                wait(5)
+                coroutine.resume(co)
         end
     end)
 
